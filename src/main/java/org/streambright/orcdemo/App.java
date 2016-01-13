128dd802a8fc493d40fca08cb2665aaa264a514c
Istvan Szukacs <leccine@gmail.com>
2016-01-13 15:25:40 +0100

    Removing fs from import

--- src/main/java/org/streambright/orcdemo/App.java
+++ src/main/java/org/streambright/orcdemo/App.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
 
 import org.apache.hadoop.conf.Configuration;
 
-import org.apache.hadoop.fs.FileSystem;
+//import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 import java.io.IOException;

e501e2089214d0535c8bb671d56a1c0999098402
Istvan Szukacs <leccine@gmail.com>
2016-01-13 15:25:06 +0100

    Minor updates

--- src/main/java/org/streambright/orcdemo/App.java
+++ src/main/java/org/streambright/orcdemo/App.java
@@ -1,8 +1,14 @@
 package org.streambright.orcdemo;
 
-import org.apache.hadoop.hive.ql.io.orc.*;
-import org.apache.hadoop.hive.serde2.objectinspector.*;
+import org.apache.hadoop.hive.ql.io.orc.Writer;
+import org.apache.hadoop.hive.ql.io.orc.OrcFile;
+import org.apache.hadoop.hive.ql.io.orc.CompressionKind;
+
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+
 import org.apache.hadoop.conf.Configuration;
+
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
@@ -13,6 +19,8 @@ public class App {
 
     private static Configuration conf = new Configuration();
     public static Writer writer;
+    public static OrcFile.Version vers = OrcFile.Version.V_0_12;
+    public static CompressionKind compr = CompressionKind.ZLIB;
 
     public static class OrcRow {
         int col1;
@@ -28,15 +36,15 @@ public class App {
 
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
 
-        String path = "/tmp/orcfile1";
+        String path = "/tmp/orcfile.orc";
 
         try {
 
             conf = new Configuration();
-            FileSystem fs = FileSystem.getLocal(conf);
+            //FileSystem fs = FileSystem.getLocal(conf);
 
             ObjectInspector ObjInspector = ObjectInspectorFactory.getReflectionObjectInspector(OrcRow.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
-            writer = OrcFile.createWriter(new Path(path), OrcFile.writerOptions(conf).inspector(ObjInspector).stripeSize(100000).bufferSize(10000).compress(CompressionKind.ZLIB).version(OrcFile.Version.V_0_12));
+            writer = OrcFile.createWriter(new Path(path), OrcFile.writerOptions(conf).inspector(ObjInspector).stripeSize(100000).bufferSize(10000).compress(compr).version(vers));
 
             writer.addRow(new OrcRow(1, "hello", "orcFile"));
             writer.addRow(new OrcRow(2, "hello2", "orcFile2"));
